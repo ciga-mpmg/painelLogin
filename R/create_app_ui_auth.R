64e00b0ddd35a3aa1e6461a82da22c6a825364e7
Fernando Corrêa <fptcorrea@gmail.com>
2023-09-01 13:35:27 -0300

    atualiza create_app_ui_auth pra atender o ambiente de produção

--- R/create_app_ui_auth.R
+++ R/create_app_ui_auth.R
@@ -4,15 +4,21 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
-create_app_ui_auth <- function(request, ui) {
+create_app_ui_auth <- function(request, ui, prod = FALSE) {
 
   function(request){
-    pass <- aecomLogin:::validate_session(request)
+    pass <- aecomLogin:::validate_session(request, prod)
 
+    if(prod){
+      replace_js <- 'location.replace("https://painel.ciga-mpmg.com.br");'
+    } else {
+      replace_js <- 'location.replace("https://painel-homolog.ciga-mpmg.com.br");'
+    }
+    
     if(pass == "true"){
       ui()
     } else {
-      shiny::tags$script(shiny::HTML('location.replace("https://painel-homolog.ciga-mpmg.com.br");'))
+      shiny::tags$script(shiny::HTML(replace_js))
     }
   }
 
