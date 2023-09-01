f150546cbbe6a77c953b6e238c6d7bece33c857a
Fernando Corrêa <fptcorrea@gmail.com>
2023-09-01 13:31:20 -0300

    criando versão de produção

--- R/validate_session.R
+++ R/validate_session.R
@@ -1,10 +1,17 @@
 validate_session <- function(req,
-                             auth_url = "https://us-central1-aecom-371410.cloudfunctions.net/"){
+                             auth_url = "https://us-central1-aecom-371410.cloudfunctions.net/", prod = FALSE){
   if(!is.na(req$HEADERS['cookie'])){
     par <- parseCookies(req$HEADERS['cookie'])
 
+  if(prod){
+    validation_endpoint = "validateToken?token="
+  } else {
+    validation_endpoint = "validateTokenProd?token="
+  }
+  
+    
     final <- try(
-      httr::GET(paste0(auth_url, "validateToken?token=", par$painel_ciga_token)) |> httr::content("text"),
+      httr::GET(paste0(auth_url, validation_endpoint, par$painel_ciga_token)) |> httr::content("text"),
       "Token invalido")
   } else {
     final <- "Token invalido"
